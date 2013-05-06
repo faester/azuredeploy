28b33dc0fc09b05ac594fd3294e482e67ccbfe13
Morten Hjorth Fæster <faester@gmail.com>
2013-05-06 12:13:22 +0200

    Removed web api config stuff.

--- AzureDeploy/AzureDummy/Global.asax.cs
+++ AzureDeploy/AzureDummy/Global.asax.cs
@@ -16,7 +16,6 @@ namespace AzureDummy
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
